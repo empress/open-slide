afe279a3206de6f990b506d2fc1dbdb627e959c8
Chris Manson <chris@manson.ie>
2022-01-26 11:32:09 +0000

    implement slide generation every 005 slides

--- blueprints/slide/index.js
+++ blueprints/slide/index.js
@@ -3,6 +3,11 @@
 const { readdirSync } = require('fs');
 const { join } = require('path');
 
+// https://stackoverflow.com/a/18953446/690826
+function round5(x) {
+  return Math.ceil(x/5)*5;
+}
+
 module.exports = {
   description: 'A simple blueprint to create a new slide markdown',
 
@@ -18,8 +23,11 @@ module.exports = {
 
         let highestNumber = Math.max(...numbers, 0);
 
-        // Logic to determine value goes here
-        return (highestNumber + 1).toString().padStart(3, '0');
+        // we want to get the next highest number that is divisible by 5
+        // this means that if you add slides one after another you will get slide
+        // 005 010 015 which gives you some space to move slides into the gaps
+        // and makes moving slides less tedious.
+        return (round5(highestNumber + 1)).toString().padStart(3, '0');
       }
     };
   },
